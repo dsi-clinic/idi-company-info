7ba5ad9e042cbed1835b08a7db69384a0bd59e37
Nikki Tebaldi <17799906+nikki-t@users.noreply.github.com>
2026-02-20 16:27:06 -0600

    Provide the ability to change file mode in storage and remove extra files

--- src/ftm2j/common/storage.py
+++ src/ftm2j/common/storage.py
@@ -8,24 +8,27 @@ import tempfile
 import smart_open
 
 
-def load_json(file_path: str) -> dict | list:
+def load_json(file_path: str, mode: str = "r") -> dict | list:
     """Loads a JSON file from the given path.
 
     Args:
         file_path: The path to the JSON file.
+        mode: The mode to open the file in.
 
     Returns:
         The JSON data loaded from the file as a dictionary or list.
     """
-    with smart_open.open(file_path) as f:
+    with smart_open.open(file_path, mode=mode) as f:
         json_data = json.load(f)
     return json_data
 
 
-def save_json(file_path: str, data: dict | list) -> None:
+def save_json(file_path: str, data: dict | list, mode: str = "w") -> None:
     """Saves a JSON file to the given path.
 
     Efficient writing: https://github.com/piskvorky/smart_open/blob/develop/howto.md#how-to-write-to-s3-efficiently
+
+    Can write in append mode for local files, S3 files are always overwritten.
 
     Args:
         file_path: The path to the JSON file.
@@ -37,5 +40,5 @@ def save_json(file_path: str, data: dict | list) -> None:
             with smart_open.open(file_path, "w", transport_params=tp) as fout:
                 json.dump(data, fout, indent=2)
     else:
-        with smart_open.open(file_path, "w") as fout:
+        with smart_open.open(file_path, mode) as fout:
             json.dump(data, fout, indent=2)
