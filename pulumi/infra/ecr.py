68bfdd5b5f81447ff799ab4e18752525582ce527
Nikki Tebaldi <17799906+nikki-t@users.noreply.github.com>
2026-03-09 11:15:40 -0500

    Update sequential execution of action workflow and create ECR repository

--- pulumi/infra/ecr.py
+++ pulumi/infra/ecr.py
@@ -1,6 +1,8 @@
 """ECR image URIs for CI-pushed containers."""
 
 import pulumi
+import pulumi_aws as aws
+
 
 from . import config
 
@@ -10,6 +12,9 @@ from . import config
 ecr_registry = pulumi.Output.from_input(config.caller.account_id).apply(
     lambda aid: f"{aid}.dkr.ecr.{config.aws_region}.amazonaws.com"
 )
+
+ecr_repo = aws.ecr.Repository("idi-ecr-orchestrator", name=f"{config.name_prefix}-company-info-orchestrator")
+
 orchestrator_image = ecr_registry.apply(
     lambda r: f"{r}/{config.name_prefix}-company-info-orchestrator:latest"
 )
